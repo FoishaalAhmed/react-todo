94a20f5d282b778e6d7e1f2d62f40cd0c2f891e3
Foisal Ahmed <foisal.cse@gmail.com>
2025-08-20 21:26:50 +0600

    feat::loop through all todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
+import React , { useState } from "react";
 import Todos from "./Components/Todos"
 import NewTodo from "./Components/NewTodo"
 
 function App() {
+
+    const [todos, setTodos] = useState([
+        {
+            id: 1,
+            text: "Learn React",
+            isCompleted: true
+        },
+        {
+            id: 2,
+            text: "Learn Vue",
+            isCompleted: false
+        },
+        {
+            id: 3,
+            text: "Learn Angular",
+            isCompleted: false
+        }
+    ]);
+
+    const onComplete = (id) => {
+        setTodos(todos.map((todo) => todo.id === id ? {...todo, isCompleted: !todo.isCompleted} : todo))
+    }
+
+    const onDelete = (id) => {
+        setTodos(todos.filter((todo) => todo.id !== id))
+    }
 
   return (
     <>
@@ -9,14 +36,12 @@ function App() {
             <div className="h-screen w-screen flex flex-col items-center justify-center space-y-4">
                 <div className="bg-white rounded-2xl shadow-md w-96 p-6">
                     
-                    <NewTodo />
+                    <NewTodo newTodo={setTodos} />
 
                     <hr className="mb-4 border-blue-500"/>
 
                     <ul className="space-y-3">
-                        <Todos />
-                        <Todos />
-                        <Todos />
+                        <Todos todos={todos} onComplete={onComplete} onDelete={onDelete}/>
                     </ul>
                 </div>
                 <p className="text-gray-700">Simple todo created by <span className="font-bold">Foisal</span></p>
--- src/Components/Todos.jsx
+++ src/Components/Todos.jsx
@@ -1,13 +1,16 @@
-export default function Todos() {
+export default function Todos({todos, onComplete, onDelete}) {
     return (
         <>
-            <li className="flex items-center justify-between group">
-                <div className="flex items-center">
-                    <input type="checkbox" className="w-5 h-5 rounded" />
-                    <span className="ml-3 text-gray-700">Nothing to do</span>
-                </div>
-                <span className="hidden group-hover:inline-block text-white text-xs font-bold uppercase cursor-pointer bg-red-500 p-1 rounded">remove</span>
-            </li>
+            {todos.length > 0 ? todos.map((todo) => (
+                <li key={todo.id} className="flex items-center justify-between group">
+                    <div className="flex items-center">
+                        <input type="checkbox" className="w-5 h-5 rounded" checked={todo.isCompleted} onClick={() => onComplete(todo.id)}/>
+                        <span className={`ml-3 text-gray-700 ${todo.isCompleted && 'line-through'}`}>{todo.text}</span>
+                    </div>
+                    <span onClick={() => onDelete(todo.id)} className="hidden group-hover:inline-block text-white text-xs font-bold uppercase cursor-pointer bg-red-500 p-1 rounded">remove</span>
+                </li>
+            )) : ''}
+            
         </>
     )
 }
